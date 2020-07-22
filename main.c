3336a649cee42614c4ccff6caed2ca20b0d5a90e
Philippe <lipeph99@gmail.com>
2020-07-22 15:55:50 +0000

    Add Extrema Coleta

--- main.c
+++ main.c
@@ -5,16 +5,16 @@ int main(void) {
 	lanc = 0;
 	esp = 0;
 	scanf("%d",&total);
-	while(((lanc*25 + esp*15) > (int)(total * 0.625 + 5)) || ((lanc*25 + esp*15) < (int)(total * 0.625 - 5))){
-		while((lanc*25 + esp*15) < (total * 0.625 - 5)){
+	while(((lanc*25 + esp*15) > (int)(total * 0.5769 + 5)) || ((lanc*25 + esp*15) < (int)(total * 0.5769 - 5))){
+		while((lanc*25 + esp*15) < (total * 0.5769 - 5)){
 			lanc++;
 			esp++;
 		}
-		if((lanc*25 + esp*15) > (int)(total * 0.625 + 5)){
+		if((lanc*25 + esp*15) > (int)(total * 0.5769 + 5)){
 			esp--;
 		}
 	}
-	printf ("Total:%d, Peq Col:%d\n",total,(int)(total * 0.625));
+	printf ("Total:%d, Peq Col:%d\n",total,(int)(total * 0.5769));
 	printf ("Lanc:%d, Esp:%d\n",lanc,esp);
 	printf ("Real:%d\n\n",(lanc*25 + esp*15));
 	
@@ -22,18 +22,37 @@ int main(void) {
 
 	lanc = 0;
 	esp = 0;
-	while(((lanc*25 + esp*15) > (int)(total * 0.25 + 5)) || ((lanc*25 + esp*15) < (int)(total * 0.25 - 5))){
-		while((lanc*25 + esp*15) < (total * 0.25 - 5)){
+	while(((lanc*25 + esp*15) > (int)(total * 0.2308 + 5)) || ((lanc*25 + esp*15) < (int)(total * 0.2308 - 5))){
+		while((lanc*25 + esp*15) < (total * 0.2308 - 5)){
 			lanc++;
 			esp++;
 		}
-		if((lanc*25 + esp*15) > (int)(total * 0.25 + 5)){
+		if((lanc*25 + esp*15) > (int)(total * 0.2308 + 5)){
 			esp--;
 		}
 	}
 
 	
-	printf ("Total:%d, Med Col:%d\n",total,(int)(total * 0.25));
+	printf ("Total:%d, Med Col:%d\n",total,(int)(total * 0.2308));
+	printf ("Lanc:%d, Esp:%d\n",lanc,esp);
+	printf ("Real:%d\n",(lanc*25 + esp*15));
+
+  //---------------------
+
+	lanc = 0;
+	esp = 0;
+	while(((lanc*25 + esp*15) > (int)(total * 0.1154 + 5)) || ((lanc*25 + esp*15) < (int)(total * 0.1154 - 5))){
+		while((lanc*25 + esp*15) < (total * 0.1154 - 5)){
+			lanc++;
+			esp++;
+		}
+		if((lanc*25 + esp*15) > (int)(total * 0.1154 + 5)){
+			esp--;
+		}
+	}
+
+	
+	printf ("Total:%d, Grd Col:%d\n",total,(int)(total * 0.1154));
 	printf ("Lanc:%d, Esp:%d\n",lanc,esp);
 	printf ("Real:%d\n",(lanc*25 + esp*15));
 	
